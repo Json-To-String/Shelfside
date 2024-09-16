18af9e17c90c9efc74f7fceef2b868b5151382cc
Json-To-String <nickai816@gmail.com>
2024-09-16 14:45:57 -0700

    some good streamlit progress - lets split this up into branches though

--- personality-quiz/home.py
+++ personality-quiz/home.py
@@ -7,17 +7,43 @@ pg = st.navigation([
                     st.Page('page_2.py')])
 pg.run()
 
+
+# class Handler():
+#
+#     def __init__(self):
+#         self.personas
+personas = ['Blue', 'Red', 'Clear',
+            'Black', 'White', 'Green',
+            'Yellow', 'Purple', 'Natural', 'Parchment']
 question_options = [
-        'Highly Disagree', 
-        'Disagree', 
-        'Slightly Disagree', 
-        'Neutral', 
-        'Slightly Agree', 
-        'Agree', 
+        'Highly Disagree',
+        'Disagree',
+        'Slightly Disagree',
+        'Neutral',
+        'Slightly Agree',
+        'Agree',
         'Highly Agree'
         ]
 
 question_values = [1, 2, 3, 4, 5, 6, 7]
 
-question_mapping = dict(map(lambda i,j : (i,j) , question_options, question_values)) 
+question_mapping = dict(map(lambda i,j : (i,j) , question_options, question_values))
 
+question_dict = {}
+question_list = []
+question_weights = []
+with open('questions.txt', 'r') as q:
+    for ind, line in enumerate(q):
+        if ind % 2 == 0:
+            question_list.append(line)
+        else:
+            question_weights.append(line)
+
+
+for persona in personas:
+    if persona not in st.session_state:
+        st.session_state[persona] = 0
+
+st.session_state
+for i in range(5):
+    print(question_list[i])
--- personality-quiz/page_2.py
+++ personality-quiz/page_2.py
@@ -2,19 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+
 question_options = [
-        'Highly Disagree', 
-        'Disagree', 
-        'Slightly Disagree', 
-        'Neutral', 
-        'Slightly Agree', 
-        'Agree', 
+        'Highly Disagree',
+        'Disagree',
+        'Slightly Disagree',
+        'Neutral',
+        'Slightly Agree',
+        'Agree',
         'Highly Agree'
         ]
 
 question_values = [1, 2, 3, 4, 5, 6, 7]
 
-question_mapping = dict(map(lambda i,j : (i,j) , question_options, question_values)) 
+question_mapping = dict(map(lambda i,j : (i,j) , question_options, question_values))
 
 #print(question_mapping)
 
@@ -33,16 +35,22 @@ div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {
     </style>
     """, unsafe_allow_html=True)
 
-with open('questions.txt', 'r') as q:
-    for line in q:
-        st.radio(f'Question {line}', question_options)
+st.session_state
 
-        ## This seems like not amazing practice but it'll change the radio labelsize
-        st.markdown(
-        """<style>
-        div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {
-        font-size: 22px;
-}
-    </style>
-    """, unsafe_allow_html=True)
-
+# with open('questions.txt', 'r') as q:
+#     for line in q:
+#         st.radio(f'Question {line}', question_options)
+#         answer = st.select_slider(
+#                 f'Question {line}',
+#                 options = question_options,
+#                 )
+#
+#
+#         ## This seems like not amazing practice but it'll change the radio labelsize
+#         st.markdown(
+#         """<style>
+#         div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {
+#         font-size: 22px;
+# }
+#     </style>
+#     """, unsafe_allow_html=True)
