2f0a9cabcbf14fb8cafcf449cefcbc0fe06f5afb
Json-To-String <nickai816@gmail.com>
2024-09-26 17:46:32 -0700

    nvm

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import numpy as np
 import os
 
 # here is where the user can switch between pages (only run in home or main.py)
-# pg = st.navigation([
-#                    st.Page('personality-quiz/start_page.py'),
-#                    st.Page('personality-quiz/question_page1.py'),
-#                    st.Page('personality-quiz/question_page2.py'),
-#                    st.Page('personality-quiz/question_page3.py'),
-#                    st.Page('personality-quiz/question_page4.py'),
-#                    st.Page('personality-quiz/question_page5.py'),
-#                    st.Page('personality-quiz/results_page.py')
-#                    # st.Page('personality-quiz/recommend_page.py')
-#                    # st.Page('personality-quiz/results_page.py')
-#                ])
-#
-# pg.run()
+pg = st.navigation([
+                   st.Page('personality-quiz/start_page.py'),
+                   st.Page('personality-quiz/question_page1.py'),
+                   st.Page('personality-quiz/question_page2.py'),
+                   st.Page('personality-quiz/question_page3.py'),
+                   st.Page('personality-quiz/question_page4.py'),
+                   st.Page('personality-quiz/question_page5.py'),
+                   st.Page('personality-quiz/results_page.py')
+                   # st.Page('personality-quiz/recommend_page.py')
+                   # st.Page('personality-quiz/results_page.py')
+               ])
+
+pg.run()
 
 st.markdown(
     """<style>
